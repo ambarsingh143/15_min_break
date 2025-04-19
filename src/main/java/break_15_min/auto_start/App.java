95a32a147c042324aba3325adf6d24a206d12a17
Ambar <ambar.singh@snva.com>
2025-04-19 10:01:48 +0530

    Changes

---
+++ src/main/java/break_15_min/auto_start/App.java
@@ -0,0 +1,13 @@
+package break_15_min.auto_start;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
